9d2842d0f0feeb22f34d01b391214006e0f95794
Gabe D <gabe.p.dominguez@gmail.com>
2022-04-06 03:15:26 +0000

    Adds shapes quiz

--- main.js
+++ main.js
@@ -127,6 +127,34 @@ const animals = [
   }
 ]
 
+const shapes = [
+  {
+    name: "ka nananam",
+    url: "https://cdn.pixabay.com/photo/2016/04/07/22/41/icons-1315027_1280.jpg",
+    answer: "shapes",
+  },
+  {
+    name: "koelai",
+    url: "http://www.clipartbest.com/cliparts/acq/o5K/acqo5K8zi.jpeg",
+    answer: "circle"
+  },
+  {
+    name: "vahi chakariakame",
+    url: "https://img.freepik.com/free-vector/neon-light-frame_53876-93482.jpg?size=338&ext=jpg&uid=P57244170",
+    answer: "triangle"
+  },
+  {
+    name: "naiki chakariakame",
+    url: "https://thndl.com/images/5_2.jpg",
+    answer: "square"
+  },
+  {
+    name: "woi katana teeve",
+    url: "https://us.123rf.com/450wm/cgdeaw/cgdeaw1904/cgdeaw190400338/121202493-red-rectangle-basic-simple-shapes-isolated-on-white-background-geometric-rectangle-icon-2d-shape-sym.jpg?ver=6",
+    answer: "rectangle"
+  }
+]
+
 let answerText = document.querySelector(".answerText");
 let shuffledArray = []
 let currentPosition = 0
@@ -167,19 +195,19 @@ function initColor(title){
   }
 }
 
-function initAnimals(title){
+function initQuiz(title, questions){
   document.querySelector(".title").innerHTML = title
-  shuffleArray(animals)
+  shuffleArray(questions)
   let yaqui_name;
 
   promptQuestion = () => {
-    let currentAnimal = shuffledArray[currentPosition]
+    let currentQuestion = shuffledArray[currentPosition]
     answerText.innerHTML = '';
-    yaqui_name = currentAnimal.name;
-    answerText.innerHTML = yaqui_name.replaceAll(/[A-Za-z]/g, "_ ");
+    yaqui_name = currentQuestion.name;
+    answerText.innerHTML = yaqui_name.replaceAll(/\s/g, "    ").replaceAll(/[A-Za-z]/g, "_ ");
     answer.classList.remove("hidden")
     circle.classList.add("anim");
-    circle.style.backgroundImage = `url('${currentAnimal.url}')`;
+    circle.style.backgroundImage = `url('${currentQuestion.url}')`;
     circle.style.backgroundSize = "cover";
     setTimeout(() => circle.classList.remove("anim"),1000);
   }
